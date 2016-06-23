9dc07a017224aaf8709c97b619587c38925920b1
Mathias Vetsch <m.vetsch@gmail.com>
2016-06-23 10:24:18 +0200

    added license to module

--- src/org/sleuthkit/autopsy/modules/goldenimage/GoldenImageIngestModuleIngestJobSettingsPanel.java
+++ src/org/sleuthkit/autopsy/modules/goldenimage/GoldenImageIngestModuleIngestJobSettingsPanel.java
@@ -1,33 +1,3 @@
-/*
- * Sample module ingest job settings panel in the public domain.  
- * Feel free to use this as a template for your module ingest job settings
- * panels.
- * 
- *  Contact: Brian Carrier [carrier <at> sleuthkit [dot] org]
- *
- *  This is free and unencumbered software released into the public domain.
- *  
- *  Anyone is free to copy, modify, publish, use, compile, sell, or
- *  distribute this software, either in source code form or as a compiled
- *  binary, for any purpose, commercial or non-commercial, and by any
- *  means.
- *  
- *  In jurisdictions that recognize copyright laws, the author or authors
- *  of this software dedicate any and all copyright interest in the
- *  software to the public domain. We make this dedication for the benefit
- *  of the public at large and to the detriment of our heirs and
- *  successors. We intend this dedication to be an overt act of
- *  relinquishment in perpetuity of all present and future rights to this
- *  software under copyright law.
- *  
- *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- *  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- *  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- *  IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
- *  OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
- *  ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- *  OTHER DEALINGS IN THE SOFTWARE. 
- */
 package org.sleuthkit.autopsy.modules.goldenimage;
 
 import java.awt.event.ActionEvent;
@@ -36,9 +6,9 @@ import java.util.logging.Level;
 import javax.swing.DefaultComboBoxModel;
 import org.sleuthkit.autopsy.casemodule.Case;
 import org.sleuthkit.autopsy.ingest.IngestJobSettingsPanel;
-import org.sleuthkit.datamodel.Content;
 import org.sleuthkit.autopsy.ingest.IngestModuleIngestJobSettings;
 import org.sleuthkit.autopsy.ingest.IngestModuleIngestJobSettingsPanel;
+import org.sleuthkit.datamodel.Content;
 import org.sleuthkit.datamodel.TskCoreException;
 
 /**
